bce20d520a5f276e4ba3aa79da3be98028c9f21d
Marcello Joaquim <marcellojoaquim1@hotmail.com>
2025-12-24 16:40:05 -0300

    iniciando projeto

---
+++ src/test/java/com/mjdsilva/localizacao/LocalizacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjdsilva.localizacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalizacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
